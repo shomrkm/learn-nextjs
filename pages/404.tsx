d3be105546758291e53e7bde088e2d226f948ea2
shomrkm <shochan1985@gmail.com>
2023-01-03 11:31:25 +0900

    feat: Customize error pages.

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import utilStyles from '../styles/utils.module.css';
+
+const Custom404: FC = () => {
+  return <h1 className={utilStyles.headingXl}>404 Not Found</h1>;
+};
+
+export default Custom404;
---
+++ pages/500.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import utilStyles from '../styles/utils.module.css';
+
+const Custom500: FC = () => {
+  return <h1 className={utilStyles.headingXl}>500 Internal Server Error</h1>;
+};
+
+export default Custom500;
